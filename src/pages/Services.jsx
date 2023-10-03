612a01dc9e5e45c0a33d4bddf2d22d5413f85dd8
Alicia <aliciappak@gmail.com>
2023-10-03 09:39:41 -0300

    added vercel.json file

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,12 @@
 import HeroServices from "../components/Services/HeroServices";
 import ServicesSection from "../components/Services/ServicesSection";
+//import ThreeD from "../components/Services/ThreeD";
 
 const Services = () => {
   return (
     <>
       <HeroServices />
+      {/* <ThreeD /> */}
       <ServicesSection />
     </>
   );
